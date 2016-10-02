d114c0adc09ae54ef0e159d987254b022c6e627e
Eric Nichols <underspecified@gmail.com>
2016-10-02 11:20:41 +0900

    modularized MonadTrans, added docstrings, added Monad.pure

--- genmonads/Monad.py
+++ genmonads/Monad.py
@@ -1,82 +1,144 @@
 import re
 import sys
 
-from pony.orm.asttranslation import *
 from pony.orm.decompiling import decompile
+
+from genmonads.MonadTrans import ast2src
 
 
 class Monad(object):
+    """
+    A base class for representing monads.
+
+    Monadic computing is supported with map, flat_map, and flatten functions, and for-comprehensions can be formed by
+    evaluating generators over monads with the mfor function.
+    """
+
     def __iter__(self):
-        raise NotImplementedError
+        """
+        Returns:
+            MonadIter: an iterator over the content of the monad to support usage in generators
+        """
+        return MonadIter(self)
 
     def __mname__(self):
+        """
+        Returns:
+            str: the monad's name
+        """
         return 'Monad'
 
     def __rshift__(self, f):
+        """
+        A symbolic alias for flat_map.
+
+        Args:
+            f (Callable[[A],Monad[B]]): the function to apply
+
+        Returns:
+            Monad[B]
+        """
         return self.flat_map(f)
 
-    def flat_map(self, f):
-        raise NotImplementedError
-
-    def map(self, f):
-        raise NotImplementedError
-
     def filter(self, f):
+        """
+        Args:
+            f (Callable[[T],bool]): the predicate
+
+        Returns:
+            Option[T]: this instance if the predicate is true when applied to its inner value, Nothing otherwise
+        """
+        raise NotImplementedError
+
+    def flat_map(self, f):
+        """
+        Applies a function that produces an Monad from unwrapped values to an Monad's inner value and flattens the
+        nested result. Equivalent to self.map(f).flatten()
+
+        Args:
+            f (Callable[[A],Monad[B]]): the function to apply
+
+        Returns:
+            Monad[B]: the resulting monad
+        """
         raise NotImplementedError
 
     def flatten(self):
+        """
+        Flattens nested instances of Monad. Equivalent to self.flat_map(lambda x: Monad.pure(x))
+
+        Returns:
+            Monad[T]: the flattened monad
+        """
+        raise NotImplementedError
+
+    def map(self, f):
+        """
+        Applies a function to the inner value of a monad.
+
+        Args:
+            f (Callable[[A],B]): the function to apply
+
+        Returns:
+            Monad[B]: the resulting monad
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def pure(value):
+        """
+        Injects a value into the monad.
+
+        Args:
+            value (T): the value
+
+        Returns:
+            Monad[T]: the monadic value
+        """
         raise NotImplementedError
 
 
 class MonadIter(object):
+    """
+    An iterator wrapper class over the content of the monad to support usage in generators
+    """
     def __init__(self, monad):
         self.monad = monad
 
     def __next__(self):
-        raise(TypeError('Use mfor(...) function for iteration' % self.monad.__mname__))
+        raise(TypeError('Use mfor(...) function for evaluation'))
 
     next = __next__
 
 
-# noinspection PyPep8Naming,PyMethodMayBeStatic
-class MonadTranslator(PythonTranslator):
-    def postGenExpr(self, node):
-        return node.code.src
-
-    def postGenExprInner(self, node):
-        qual_src = [qual.src for qual in node.quals]
-        if qual_src:
-            qual_src[-1] = qual_src[-1].replace('.flat_map', '.map')
-        return ''.join(qual_src) + node.expr.src + ')' * len(node.quals)
-
-    def postGenExprFor(self, node):
-        src = node.iter.src
-        src += ''.join('.filter(lambda %s: %s)' % (node.assign.src, if_.test.src) for if_ in node.ifs)
-        src += '.flat_map(lambda %s: ' % node.assign.src
-        return src
-
-    def postLambda(self, node):
-        #print('postLambda:', node, file=sys.stderr)
-        src = 'lambda %s: %s' % (','.join(node.argnames), node.code.src)
-        #print('src:', src, file=sys.stderr)
-        return src
-
-
-def ast2src(tree):
-    MonadTranslator(tree)
-    return tree.src
-
-
 # noinspection PyShadowingBuiltins,PyProtectedMember
 def mfor(gen, frame_depth=1):
+    """The monadic for-comprehension
+    Evaluates a generator over a monadic value, translating it into the equivalent for-comprehension.
+
+    Args:
+        gen (Generator[T, None, None]): a generator over a monadic value
+        frame_depth (int): the frame depth of which to search for the outermost monad
+
+    Returns:
+        Monad[T]: the for-comprehension that corresponds to the generator
+    """
     try:
+        # decompile the generator into AST, externally referenced names, and memory cells
         ast_, external_names, cells = decompile(gen)
         #print('ast:', ast_, file=sys.stderr)
         #print('external_names:', external_names, file=sys.stderr)
         #print('globals:', gen.gi_frame.f_globals, file=sys.stderr)
+
+        # for generator expressions, the outmost monad is evaluated, converting it into a MonadIter
+        # it is referred to by the symbol .0 in the generator's bytecode, so we need to retrieve the original monad
+        # for generator functions, the original monad is preserved in unevaluated form, so we use the monad class as
+        # a dummy value
         monad = gen.gi_frame.f_locals['.0'].monad if '.0' in gen.gi_frame.f_locals else Monad
         code = re.sub(r'''^\.0''', 'monad', ast2src(ast_))
         #print('code:', code, file=sys.stderr)
+
+        # next we insert the original monad into the code's locals and return the results of its evaluation
         globals = sys._getframe(frame_depth).f_globals
         locals = sys._getframe(frame_depth).f_locals
         locals['monad'] = monad
@@ -85,4 +147,15 @@ def mfor(gen, frame_depth=1):
         raise e
 
 
-do = mfor
+def do(gen, frame_depth=1):
+    """A synonym for mfor
+    Evaluates a generator over a monadic value, translating it into the equivalent for-comprehension.
+
+    Args:
+        gen (Generator[T, None, None]): a generator over a monadic value
+        frame_depth (int): the frame depth of which to search for the outermost monad
+
+    Returns:
+        Monad[T]: the for-comprehension that corresponds to the generator
+    """
+    mfor(gen, frame_depth)
---
+++ genmonads/MonadTrans.py
@@ -0,0 +1,104 @@
+from pony.orm.asttranslation import *
+# noinspection PyUnresolvedReferences
+from pony.thirdparty.compiler.ast import *
+
+
+# noinspection PyPep8Naming,PyMethodMayBeStatic
+class MonadTranslator(PythonTranslator):
+    # noinspection PyUnresolvedReferences
+    """
+    The translator class that converts the AST of a generator over monads into a series of nested calls to flat_map,
+    map, and filter.
+
+    For example, consider the following generator:
+
+    >>> from genmonads.Option import *
+    >>> print(mfor(x + y
+              for x in option(2)
+              if x < 10
+              for y in option(5)
+              if y % 2 != 0))
+    Some(7)
+
+    The above generator is automatically translated into the following at run-time:
+
+    >>> print(option(2)
+                  .filter(lambda x: x < 10)
+                  .flat_map(lambda x: option(5)
+                      .filter(lambda y: y % 2 != 0)
+                      .map(lambda y: x + y)))
+    Some(7)
+    """
+
+    def postGenExpr(self, node):
+        """
+        Converts the AST node for the full generator expression into its code representation. Responsible for
+        discarding the parentheses around the generator expression.
+
+        Args:
+            node (GenExpr): the AST node
+
+        Returns:
+            str: the source code representation
+        """
+        return node.code.src
+
+    def postGenExprInner(self, node):
+        """
+        Converts the AST node for the generator's inner expression into its code representation. Responsible for
+        converting the final flat_map call into a call to map and adding closing parentheses.
+
+        Args:
+            node (GenExprInner): the AST node
+
+        Returns:
+            str: the source code representation
+        """
+        qual_src = [qual.src for qual in node.quals]
+        if qual_src:
+            qual_src[-1] = qual_src[-1].replace('.flat_map', '.map')
+        return ''.join(qual_src) + node.expr.src + ')' * len(node.quals)
+
+    def postGenExprFor(self, node):
+        """
+        Converts the AST node for the body of a generator's for statements into its code representation.
+        Responsible for converting if statements into calls to filter and "for x in y" in to calls to flat_map.
+
+        Args:
+            node (GenExprFor): the AST node
+
+        Returns:
+            str: the source code representation
+        """
+        src = node.iter.src
+        src += ''.join('.filter(lambda %s: %s)' % (node.assign.src, if_.test.src) for if_ in node.ifs)
+        src += '.flat_map(lambda %s: ' % node.assign.src
+        return src
+
+    def postLambda(self, node):
+        """Converts the AST node for a lambda expression into its code representation
+
+        Args:
+            node (Lambda): the AST node
+
+        Returns:
+            str: the source code representation
+        """
+        src = 'lambda %s: %s' % (','.join(node.argnames), node.code.src)
+        return src
+
+
+def ast2src(tree):
+    """
+    Converts an AST into python source, replacing generator expressions into a series of nested calls to flat_map,
+    map, and filter by applying the MonadTranslator to an AST.
+
+    Args:
+        tree (GenExpr): the AST node of a generator expression
+
+    Returns:
+        str: the source code representation
+    """
+    MonadTranslator(tree)
+    # noinspection PyUnresolvedReferences
+    return tree.src
